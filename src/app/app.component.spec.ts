f8528e93951a64a61dff87296c4a2053d2277e48
Rodrigo Sturm <rodrigoks@gmail.com>
2020-11-12 08:09:00 -0300

    criado jogo da velha module.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,6 @@
 import { TestBed } from '@angular/core/testing';
+import { JogoDaVelhaModule } from './jogo-da-velha/jogo-da-velha.module';
+
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
@@ -7,6 +9,9 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      imports: [
+        JogoDaVelhaModule
+      ]
     }).compileComponents();
   });
 
@@ -16,16 +21,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'jogodavelha'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('jogodavelha');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('jogodavelha app is running!');
-  });
 });
